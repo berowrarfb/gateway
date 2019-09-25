32659d73005196430fdbe148fa37c2880e9a5cfe
mvbnano <matt.vanbreugel@gmail.com>
2019-09-25 12:09:09 +1000

    WIP led breakout class

--- vl6180x.py
+++ vl6180x.py
@@ -10,16 +10,16 @@ import adafruit_vl6180x
  
  
 # Create I2C bus.
-i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
  
 # Create sensor instance.
-sensor = adafruit_vl6180x.VL6180X(i2c)
+#sensor = adafruit_vl6180x.VL6180X(i2c)
  
 # Main loop prints the range and lux every second:
-while True:
-    # Read the range in millimeters and print it.
-    range_mm = sensor.range
-    print('Range: {0}mm'.format(range_mm))
+#while True:
+#    # Read the range in millimeters and print it.
+#    range_mm = sensor.range
+#    print('Range: {0}mm'.format(range_mm))
     # Read the light, note this requires specifying a gain value:
     # - adafruit_vl6180x.ALS_GAIN_1 = 1x
     # - adafruit_vl6180x.ALS_GAIN_1_25 = 1.25x
@@ -29,7 +29,34 @@ while True:
     # - adafruit_vl6180x.ALS_GAIN_10 = 10x
     # - adafruit_vl6180x.ALS_GAIN_20 = 20x
     # - adafruit_vl6180x.ALS_GAIN_40 = 40x
-    light_lux = sensor.read_lux(adafruit_vl6180x.ALS_GAIN_1)
-    print('Light (1x gain): {0}lux'.format(light_lux))
+#    light_lux = sensor.read_lux(adafruit_vl6180x.ALS_GAIN_1)
+#    print('Light (1x gain): {0}lux'.format(light_lux))
     # Delay for a second.
-    time.sleep(1.0)
+#    time.sleep(1.0)
+
+class sensor_vl6180x():
+	'''
+	'''
+	
+	def on_activate(self):
+		'''
+		'''
+		# Create I2C bus.
+		self.i2c = busio.I2C(board.SCL, board.SDA)
+	 
+		# Create sensor instance.
+		self.sensor = adafruit_vl6180x.VL6180X(i2c)
+		
+	def get_range(self):
+		'''
+		'''
+		range_mm = sensor.range
+		return range_mm
+		
+	def get_lux(self):
+		'''
+		'''
+		light_lux = sensor.read_lux(adafruit_vl6180x.ALS_GAIN_1)
+		return light_lux
+		
+	on_activate(self)
