05d23cb2cf290eb9be1633bfadc3537177662bf5
mvbnano <matt.vanbreugel@gmail.com>
2019-10-01 15:33:07 +1000

    test devices together

---
+++ range_test.py
@@ -0,0 +1,17 @@
+import kit11588
+import vl6180x
+
+leds = kit11588.led_breakout_kit11588()
+range_device = vl6180x.sensor_vl6180x()
+
+run = True
+
+while True:
+    range = range_device.get_range()
+
+    if 0 <= range <= 100:
+        leds.set_led({'red':True, 'green':False, 'blue':False})
+    elif 100 <= range <= 200:
+        leds.set_led({'red':False, 'green':True, 'blue':False})
+    else:
+        leds.set_led({'red':True, 'green':False, 'blue':True})
