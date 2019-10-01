a108a23c5e681c86fef96e0fedde82df5bbc679d
mvbnano <matt.vanbreugel@gmail.com>
2019-10-01 15:27:32 +1000

    remove old comments

--- kit11588.py
+++ kit11588.py
@@ -1,32 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-# init
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
-
-# init pins
-#pin_led_blue = 18
-#pin_led_red = 23
-#pin_led_green = 24
-
-#GPIO.setup(pin_led_blue,GPIO.OUT)
-#GPIO.setup(pin_led_red,GPIO.OUT)
-#GPIO.setup(pin_led_green,GPIO.OUT)
-
-
-#print("LED on")
-#GPIO.output(pin_led_blue,GPIO.HIGH)
-#GPIO.output(pin_led_red,GPIO.HIGH)
-#PIO.output(pin_led_green,GPIO.HIGH)
-
-#time.sleep(1)
-
-#print("LED off")
-#GPIO.output(pin_led_blue,GPIO.LOW)
-#GPIO.output(pin_led_red,GPIO.LOW)
-#GPIO.output(pin_led_green,GPIO.LOW)
-
 class led_breakout_kit11588():
     '''
     '''
