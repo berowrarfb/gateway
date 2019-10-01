15e4cd30ce7de3cff01c1a15e323c168f93d88b3
mvbnano <matt.vanbreugel@gmail.com>
2019-10-01 15:40:53 +1000

    handle program exit

--- range_test.py
+++ range_test.py
@@ -6,12 +6,15 @@ range_device = vl6180x.sensor_vl6180x()
 
 run = True
 
-while True:
-    range = range_device.get_range()
+try:
+    while True:
+        range = range_device.get_range()
 
-    if 0 <= range <= 100:
-        leds.set_led({'red':True, 'green':False, 'blue':False})
-    elif 100 <= range <= 200:
-        leds.set_led({'red':False, 'green':True, 'blue':False})
-    else:
-        leds.set_led({'red':True, 'green':False, 'blue':True})
+        if 0 <= range <= 100:
+            leds.set_led({'red':True, 'green':False, 'blue':False})
+        elif 100 <= range <= 200:
+            leds.set_led({'red':False, 'green':True, 'blue':False})
+        else:
+            leds.set_led({'red':False, 'green':False, 'blue':True})
+except:
+    leds.set_led({'all':False})
