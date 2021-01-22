97f72476ee7fa0cd9502c6694650c1782469da76
mvbnano <matt.vanbreugel@gmail.com>
2021-01-22 13:05:49 +1100

    connected doors to station

---
+++ class_station.py
@@ -0,0 +1,9 @@
+import class_doors as doors
+
+class brfb_station:
+
+    def __init__(self):
+        self.bay_1 = doors.motor_door(1, 'Closed')
+        self.bay_2 = doors.motor_door(2, 'Closed')
+        self.bay_3 = doors.motor_door(3, 'Closed')
+
