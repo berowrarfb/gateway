f74b3ad572576d84b443d06e89b4d4965dcf808a
mvbnano <matt.vanbreugel@gmail.com>
2021-01-22 13:07:26 +1100

    main door and side door

--- class_station.py
+++ class_station.py
@@ -7,3 +7,6 @@ class brfb_station:
         self.bay_2 = doors.motor_door(2, 'Closed')
         self.bay_3 = doors.motor_door(3, 'Closed')
 
+        self.main_door = doors.regular_door('Closed')
+        self.side_door = doors.regular_door('Closed')
+
