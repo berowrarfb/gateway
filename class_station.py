3bd2f244921ccf9f3fd98e879c577aa21806bd0a
mvbnano <matt.vanbreugel@gmail.com>
2021-01-26 16:25:23 +1100

    class instance working

--- class_station.py
+++ class_station.py
@@ -2,11 +2,12 @@ import class_doors as doors
 
 class brfb_station:
 
-    def __init__(self):
-        self.bay_1 = doors.motor_door(1, 'Closed')
-        self.bay_2 = doors.motor_door(2, 'Closed')
-        self.bay_3 = doors.motor_door(3, 'Closed')
+    bay_1 = doors.motor_door(1, 'Closed')
+    bay_2 = doors.motor_door(2, 'Closed')
+    bay_3 = doors.motor_door(3, 'Closed')
 
-        self.main_door = doors.regular_door('Closed')
-        self.side_door = doors.regular_door('Closed')
+    main_door = doors.regular_door('Closed')
+    side_door = doors.regular_door('Closed')
 
+    def __init__(self, name=''):
+        self.name = name  # not required
